5d5d8bb57b50c6c6e1a407221355387e4880f3e8
mighark <migharceus@gmail.com>
2020-02-24 16:13:21 +0100

    carpetas practicas

---
+++ p1/paquete.rb
@@ -0,0 +1,86 @@
+require_relative "pedido"
+
+class Paquete
+    PesoMax = 5000
+    def initialize(id)
+        @id = id
+        @peso = 0
+        @productos = Array.new()
+        @dir = nil
+        @fragil = false
+        @refr = false
+    end
+
+    attr_reader :id, :dir, :peso
+
+    def anadirProducto(producto)
+        #si hay producto fragil, fin
+        if(@fragil)
+            return false
+        end
+
+        #si el producto es fragil y hay mas productos, fin
+        if(producto.tipo == "fragil" && @productos.length > 0)
+            #fracaso
+            return false
+        end
+
+        #si el paquete es refrigerado y el producto no, fin
+        if(@refr && producto.tipo != "alimentacion_refr")
+            #fracaso
+            return false
+        end
+
+        #si el producto es refrigerado y el paquete no, fin
+        if(producto.tipo == "alimentacion_refr" && !@refr && @productos.length > 0)
+            #fracaso
+            return false
+        end
+
+        #si cabe por peso meter
+        if(producto.peso + @peso <= PesoMax)
+            @productos.push(producto)
+            #ajustar variables de paquete
+            @peso += producto.peso
+            @dir = producto.dir
+            if producto.tipo == "fragil"
+                @fragil = true
+            elsif producto.tipo == "alimentacion_refr"
+                @refr = true
+            end
+            #exito
+            return true
+        end
+        #fracaso
+        return false
+    end
+
+    def numProductos
+        @productos.length
+    end
+
+    def to_s
+        productos = ""
+        @productos.each{|x| productos += "#{x.desc}. Peso: #{x.peso}. Tipo: #{x.tipo}"}
+        return "Id: #{@id}. Productos: " + productos
+    end
+end
+
+
+def anadirAPaquetes(paquetes, sinEntregar, producto)
+    paquetesDireccion = paquetes.select{|x| x.dir.dir == producto.dir.dir}
+    done = paquetesDireccion.inject(false){|done, x| done = done || x.anadirProducto(producto)}
+    if !done
+        paquete = Paquete.new(paquetes.length)
+        paquete.anadirProducto(producto)
+        paquetes.push(paquete)
+        sinEntregar.push(paquete)
+    end
+end
+
+class Pedido
+    def empaquetarPedido(paquetes, sinEntregar)
+        @productos.each{|x| anadirAPaquetes(paquetes, sinEntregar, x)}
+    end
+end
+
---
+++ p1/reparto.rb
@@ -0,0 +1,95 @@
+class Camion
+    def initialize(matricula, maxPaquetes, maxPeso)
+        @matricula = matricula
+        @maxPaquetes = maxPaquetes
+        @maxPeso = maxPeso
+        @repartos = Array.new()
+    end
+
+    attr_accessor :matricula, :maxPaquetes, :maxPeso
+    attr_reader :repartos
+
+    def disponibleReparto(fecha, turno)
+        @repartos.inject(true){|avlb, x| avlb = avlb && (x.fecha != fecha || x.turno != turno)}
+    end
+
+    def paquetesPosibles(paquetes)
+        nPaquetes = 0
+        peso = 0
+        nProd = 0
+        paquetesPosibles = Array.new()
+        #introduce los paquetes posibles en orden
+        paquetes.each{|x| if(nPaquetes < @maxPaquetes && peso + x.peso <= @maxPeso)
+            paquetesPosibles.push(x)
+            nPaquetes += 1
+            peso += x.peso
+            nProd += x.numProductos
+        end}
+        return [paquetesPosibles, self, nProd]
+    end
+
+    def asignarReparto(reparto)
+        @repartos.push(reparto)
+    end
+
+end
+
+class Reparto
+    def initialize(fecha, turno, paquetes)
+        @fecha = fecha
+        @turno = turno
+        @paquetes = paquetes
+        @entregados = Array.new()
+    end
+
+    attr_accessor :fecha, :turno
+
+    def verNoEntregados
+        @paquetes.each{|x| puts x}
+    end
+
+    def verEntregados
+        @entregados.each{|x| puts x}
+    end
+
+    def entregado(id)
+        paquete = @paquetes.find{|x| x.id == id}
+        if(paquete)
+            @paquetes.delete(paquete)
+            @entregados.push(paquete)
+            return true
+        else
+            return false
+        end
+    end
+
+    def to_s
+        return "Fecha: #{@fecha}. Turno: #{@turno}. Paquetes: #{@paquetes}."
+    end
+
+end
+
+def planificarReparto(fecha, turno, distrito, paquetes, camiones)
+    camionesDisponibles = camiones.select{|x| x.disponibleReparto(fecha, turno)}
+    if(camionesDisponibles.length <= 0) 
+        return nil
+    end
+    
+    paquetesDistrito = paquetes.select{|x| x.dir.distrito == distrito}
+
+    #calcula los posibles repartos de cada camion
+    #devuelve un array con la lista de paquetes, el camion y el numero de productos
+    posiblesRepartos = camionesDisponibles.collect{|x| x.paquetesPosibles(paquetesDistrito)}
+
+    #ordena la lista de mayor a menor numero de productos y toma el mayor
+    repartoFinal = posiblesRepartos.sort!{|a,b| b[2] <=> a[2]}[0]
+    #si no hay paquetes posibles, no asignar reparto
+    if(repartoFinal[0].length <= 0)
+        return nil
+    end
+
+    #asigna reparto y quita paquetes de la lista
+    repartoFinal[1].asignarReparto(Reparto.new(fecha, turno, repartoFinal[0]))
+    repartoFinal[0].each{|x| paquetes.delete(x)}
+
+end
